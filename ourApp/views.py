5d90c9e1b76f7745ceec64938e3245f46621c91b
skitinet <@nszsp08244.SZ.RT.RU>
2018-05-04 11:59:29 +0300

    cache__/urls.cpython-36.pyc   t sdgfasdfg asdfasdf asdfasdf as
    I
    
    dsfsdsdfsdfsdsdfqweqweqwqasd
    qweq
    DA
    
    1
    asd
    as
    :q
    
    :qw
    :wq
    
    iiii

--- ourApp/views.py
+++ ourApp/views.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import datetime
 
 def index(request):
 	return render(request, 'ourApp/index.html', {})
+
+def time(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html/" % now
+    return HttpResponse(html)
